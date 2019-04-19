48dba141d39d6cf68b659b2e895e5de7ba192589
Habbul <boo.hubul@gmail.com>
2019-04-19 18:40:04 +0300

    Thd

--- func.py
+++ func.py
@@ -214,7 +214,7 @@ time.sleep(50)
 
 while low_voltage+i <= high_voltage:
     uncoupling(l, gen, 5*60)
-    gen_duty_cycle(l, gen, source=1, dutycycle=80, delay=0)
+    gen_duty_cycle(l, gen, source=1, dutycycle=20, delay=0)
     set_gen_form(l, gen, func="SQU", freq=0.1, amp=(low_voltage + i), offset=(low_voltage+i)/2-0.1)
     print("GENERATING {}V".format(low_voltage + i))
     # start_gen(l, gen, source=1)
