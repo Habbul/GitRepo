03becfc2ffa8efbd1b2558f60af15b56f8961334
Habbul <boo.hubul@gmail.com>
2019-06-09 20:01:28 +0300

    FREQS_DCYCLE_80_VOLTAGE_-0.2

--- experimental_setup/func.py
+++ experimental_setup/func.py
@@ -234,7 +234,7 @@ gen_reset(gen)
 
 stop_gen(l, gen, source=1)
 
-voltages = [0.3, 0.5]
+voltages = [-0.2, 0]
 dcycles = [90, 70, 40, 20]
 freqs = [0.02, 0.1, 0.2, 0.5]
 
@@ -244,11 +244,11 @@ print("Starting experiment cycle. Switch on the supply and plug in the memristor
 time.sleep(50)
 curr = min_voltage
 step = 0.1
-for curr in dcycles:
+for curr in freqs:
     coupling(l, gen, 5*60)
     # print(curr)
-    gen_duty_cycle(l, gen, source=1, dutycycle=curr, delay=0)
-    set_gen_form(l, gen, func="SQU", freq=0.1, amp=abs(-0.2 - 0.15), offset=(-0.2 - 0.15) / 2 + 0.15 - 0.008)
+    gen_duty_cycle(l, gen, source=1, dutycycle=80, delay=0)
+    set_gen_form(l, gen, func="SQU", freq=curr, amp=abs(-0.2 - 0.15), offset=(-0.2 - 0.15) / 2 + 0.15 - 0.008)
     start_gen(l, gen, source=1)
     print("GENERATING {}".format(curr))
 
