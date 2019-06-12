902704009f71a9c9c58ceaf2e4e52c2268051a87
Habbul <boo.hubul@gmail.com>
2019-06-12 12:23:47 +0300

    RAMP

--- experimental_setup/func.py
+++ experimental_setup/func.py
@@ -193,7 +193,7 @@ rez_coef = 25 // vpd  # 50
 
 rm = visa.ResourceManager()
 rm.list_resources("?*")
-gen = rm.open_resource("USB0::0x4348::0x5537::NI-VISA-30005::RAW")
+gen = rm.open_resource("USB0::0x4348::0x5537::NI-VISA-40005::RAW")
 gen.timeout = 5000
 osc = rm.open_resource("USB0::0x0699::0x03A6::C041256::INSTR")
 print(osc)
@@ -246,17 +246,17 @@ time.sleep(50)
 curr = min_voltage
 step = 0.1
 counter = 0
-for curr in voltages:
-    # set_osc_hor(l, osc, osc_hors[counter])
+for curr in freqs:
+    set_osc_hor(l, osc, osc_hors[counter])
     counter+=1
     coupling(l, gen, 5*60)
     # print(curr)
     gen_duty_cycle(l, gen, source=1, dutycycle=10, delay=0)
-    set_gen_form(l, gen, func="REXP", freq=0.1, amp=abs(curr-0.15), offset=(curr- 0.15) / 2 + 0.15 - 0.008)
+    set_gen_form(l, gen, func="RAMP", freq=curr, amp=abs(-0.2-0.15), offset=(-0.2- 0.15) / 2 + 0.15 - 0.008)
     start_gen(l, gen, source=1)
     print("GENERATING {}".format(curr))
 
-    capture_data(l, osc, w_time=5*60, snap_period=0.5, f_name="freqs_{}.txt".format(
+    capture_data(l, osc, w_time=5*60, snap_period=0.5, f_name="freq_{}.txt".format(
         curr))
     # time.sleep(15)
     # start_gen(l, gen, source=1)
